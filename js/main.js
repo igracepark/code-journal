76f03b77998660cbfd6bf1166b4afffa741dbbe7
Grace Park <haeeunee@gmail.com>
2021-05-15 12:51:07 -0700

    Completed eventListener to update photo preview.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+
+const $photoUrl = document.querySelector("div.input-container input[name='photo-url']");
+const $photoPreview = document.querySelector('.image');
+
+$photoUrl.addEventListener('input', function (event) {
+  const currentURL = $photoPreview.getAttribute('src');
+  if ($photoUrl.value !== currentURL) {
+    $photoPreview.setAttribute('src', $photoUrl.value);
+  }
+});
