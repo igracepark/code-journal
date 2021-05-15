3d74c8098e91cff967af74b266f5591cc896de3e
Grace Park <haeeunee@gmail.com>
2021-05-15 14:08:48 -0700

    Completed putting form input values in a new object.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 /* global data */
 /* exported data */
 
-const $photoUrl = document.querySelector("div.input-container input[name='photo-url']");
+const $photoUrl = document.querySelector("div.input-container input[name='photoURL']");
 const $photoPreview = document.querySelector('.image');
+const $formSubmit = document.querySelector('#journal-entry');
 
 $photoUrl.addEventListener('input', function (event) {
   const currentURL = $photoPreview.getAttribute('src');
   if ($photoUrl.value !== currentURL) {
     $photoPreview.setAttribute('src', $photoUrl.value);
   }
+});
+
+$formSubmit.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const newEntry = {
+    title: $formSubmit.elements.title.value,
+    photoURL: $formSubmit.elements.photoURL.value,
+    notes: $formSubmit.elements.notes.value
+  };
+  return newEntry;
 });
